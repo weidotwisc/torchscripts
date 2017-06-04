37d23ffcb5dd6c44d8710c8f113a0e949952aa9d
weiz <weiz@us.ibm.com>
2017-06-04 05:34:14 -0400

    add resnet run script.

--- resnet/run.sh
+++ resnet/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# Usage: run.sh lua_scripts_dir data_dir cache_dir nGPU
+# Usage: run.sh lua_scripts_dir data_dir depth nGPU
 cmd_log=`echo $0 $@`
 echo "cmd log is ${cmd_log}"
 
 # step 1 test if there are the right number of arguments 
 if [[ "$#" -ne 4 ]]; then
-    echo "Usage: $0 lua_scripts_dir data_dir cache_dir nGPU" >&2
+    echo "Usage: $0 lua_scripts_dir data_dir depth nGPU" >&2
     exit 1
 fi 
 lua_scripts_dir=$1
 data_dir=$2
-cache_dir=$3
+depth=$3
 nGPU=$4
-bs=128
-nEpochs=55
-netType=alexnetowtbn
+bs=256
+nEpochs=90
+netType=resnet${depth}
 
 jobid=${netType}_bs${bs}_nEpochs${nEpochs}_nGPU${nGPU}
 timestamp=`date +"%Y-%m-%d-%T"`
@@ -22,13 +22,15 @@ mkdir -p ${jobid}
 pushd ${jobid}
 mkdir -p ${timestamp}
 pushd ${timestamp}
-# (th main.lua -data /nfs_weiz/mfeng/imagenet/ -cache /nfs_weiz/imgnet_cache -nGPU 4 -nDonkeys 12 -nEpochs 55 -batchSize 128 -epochSize 10000 -netType alexnetowtbn >  4L.stdout) &> 4L.stderr
-
+# th main.lua -depth 101 -nGPU 4 -nThreads 12 -batchSize 256 -shareGradInput true -data /mnt/glusterfs/GVolDist1/DEEPLEARN/torch_imagenet1K
 rm -fr log.std*
 ln -s ${lua_scripts_dir}/*.lua ./
 ln -s ${lua_scripts_dir}/models ./
+ln -s ${lua_scripts_dir}/datasets ./
+ln -s ${lua_scripts_dir}/pretrained ./
 
-cmdline="(th main.lua -data ${data_dir} -cache ${cache_dir} -nGPU ${nGPU} -nDonkeys 12 -nEpochs ${nEpochs} -batchSize ${bs} -epochSize 10000 -netType ${netType} >  log.stdout) &> log.stderr"
+
+cmdline="(th main.lua -depth ${depth} -nGPU ${nGPU} -nThreads 12 -batchSize ${bs} -shareGradInput true -data ${data_dir} > log.stdout) &> log.stderr"
 
 echo $cmd_log >> ./cmdlog
 
